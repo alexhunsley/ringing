bc129ddcab9d0271a690a1164182a57dea48da05
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2019-08-06 23:16:26 +0100

    Add x and & to grammar

--- algebraic-place-notation/code/gpn.py
+++ algebraic-place-notation/code/gpn.py
@@ -1,17 +1,24 @@
 from lark import Lark
 
-l = Lark('''pnlist: pn* 
+l = Lark('''pnlist: pnstring ("." pnstring)* ("." ENDCODE)?
+
+			pnstring: ALL_SWAP
+				  | pn* 
                   | REVERSE_PN pn*
 
             pn: HEXDIGIT
 	          | "[" SIGNED_INT "]"
 
 	        REVERSE_PN: "~"
-	        
+	        ALL_SWAP: "x" | "X"
+			ENDCODE: "&"
+
 	        %import common.HEXDIGIT
 	        %import common.SIGNED_INT
          ''', start='pnlist')
 
-print( l.parse("1458[-10]").pretty() )
-print( l.parse("~12").pretty() )
-print( l.parse("1234").pretty() )
+# print( l.parse("1458[-10]").pretty() )
+# print( l.parse("~12").pretty() )
+# print( l.parse("1234").pretty() )
+
+print( l.parse("12.34.x.78[-22].&").pretty() )
