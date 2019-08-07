adea103c1d8beb7ad626b738e42f4ff3ea8da0a0
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2019-08-07 11:18:45 +0100

    Work on GPN grammar

--- algebraic-place-notation/code/gpn.py
+++ algebraic-place-notation/code/gpn.py
@@ -1,14 +1,38 @@
 from lark import Lark
 
+#
+# examples of acceptable PN:
+#
+# x can have . around it:
+#     x.1
+#     x.1.x
+#     1.x
+#
+# x doesn't need . between it and non-x:
+#     x12
+#     x12x
+#     12x
+#
+# random:
+#     1.2.3.4
+#     12
+#     12.34
+#     12.90E
+#     x12x34.14
+#     145.56x78x12
+#     1.x.2.x.3.x
+#
+#
 
 
 # l = Lark('''pnlist: ALL_SWAP
 # 	              | pnstring? (("." pnstring | ALL_SWAP pnstring?))* 
 
-l = Lark('''pnlist: pnstring? (("." pnstring | ALL_SWAP pnstring?))* 
+l = Lark('''pnlist: pnstring ("." pnstring)* 
+                  | pnstring pnstring
 
 			pnstring: ALL_SWAP
-				  | pn* 
+				  | pn*
                   | REVERSE_PN pn*
 
             pn: HEXDIGIT
@@ -28,5 +52,12 @@ l = Lark('''pnlist: pnstring? (("." pnstring | ALL_SWAP pnstring?))*
 
 # print( l.parse("12.34.x.78[-22]").pretty() )
 # print( l.parse("12x34").pretty() )
-print( l.parse("x12x34x").pretty() )
+
+# r = l.parse("x12x34.56")
+r = l.parse("x12x")
+
+print(r)
+print()
+print(r.pretty())
+
 # print( l.parse("x").pretty() )
