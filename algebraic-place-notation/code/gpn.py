a7bfaf6117bb8b6d7d529669cbb2f06eadc92dce
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2019-08-07 09:33:57 +0100

    Add show ambiguity to earley parser, fiddle with def

--- algebraic-place-notation/code/gpn.py
+++ algebraic-place-notation/code/gpn.py
@@ -1,6 +1,11 @@
 from lark import Lark
 
-l = Lark('''pnlist: pnstring ("." pnstring)* ("." ENDCODE)?
+
+
+# l = Lark('''pnlist: ALL_SWAP
+# 	              | pnstring? (("." pnstring | ALL_SWAP pnstring?))* 
+
+l = Lark('''pnlist: pnstring? (("." pnstring | ALL_SWAP pnstring?))* 
 
 			pnstring: ALL_SWAP
 				  | pn* 
@@ -8,17 +13,20 @@ l = Lark('''pnlist: pnstring ("." pnstring)* ("." ENDCODE)?
 
             pn: HEXDIGIT
 	          | "[" SIGNED_INT "]"
-
+              
 	        REVERSE_PN: "~"
 	        ALL_SWAP: "x" | "X"
 			ENDCODE: "&"
 
 	        %import common.HEXDIGIT
 	        %import common.SIGNED_INT
-         ''', start='pnlist')
+         ''', start='pnlist', ambiguity='explicit')
 
 # print( l.parse("1458[-10]").pretty() )
 # print( l.parse("~12").pretty() )
 # print( l.parse("1234").pretty() )
 
-print( l.parse("12.34.x.78[-22].&").pretty() )
+# print( l.parse("12.34.x.78[-22]").pretty() )
+# print( l.parse("12x34").pretty() )
+print( l.parse("x12x34x").pretty() )
+# print( l.parse("x").pretty() )
