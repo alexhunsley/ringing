750ca1401477e1a44c6b0907de7790013e005493
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2020-04-18 12:43:38 +0100

    Update splitCombos.py

--- pn-exhaustive-explore/splitCombos.py
+++ pn-exhaustive-explore/splitCombos.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import math
-
 #
 # splitCombos.py
 #
@@ -102,27 +102,6 @@ import math
 poolAObjectCount = 2
 poolBObjectCount = 8
 
-# maximum places we allow in a change.
-# it's unusual to get more than 4.
-maximumPlaces = 4
-
-totalCountIncludingTreble = poolAObjectCount + poolBObjectCount + 2
-
-fullChange = '.' * totalCountIncludingTreble
-print(fullChange)
-
-totalObjects = poolAObjectCount + poolBObjectCount
-
-# we need to ensure pool A count <= pool B count as algorithm
-# assume this; swap them if necessary.
-
-if poolBObjectCount < poolAObjectCount:
-	poolAObjectCount, poolBObjectCount = poolBObjectCount, poolAObjectCount
-
-print("")
-print("Pool A size:", poolAObjectCount)
-print("Pool B size:", poolBObjectCount)
-print("")
 
 # print(math.factorial(2))
 
@@ -130,75 +109,145 @@ print("")
 #
 #   = n! / (n-r!)r!
 def calcCombos(n, r):
-	print(" =========== factorial: ", n, r)
+	# print("      =========== factorial: ", n, r)
 	return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
 
 # print(calcCombos(3, 1))
 
 
-maximumSwapsPoolsA = poolAObjectCount // 2 
-maximumSwapsPoolsB = poolBObjectCount // 2
 
-maximumSwapsEverywhere = maximumSwapsPoolsA + maximumSwapsPoolsB
-minimumSwapsEverywhere = max(0, (totalObjects - maximumPlaces) // 2)
+# maximum places we allow in a change.
+# it's unusual to get more than 4.
+def calcSplitCombosCount(poolAObjectCount, poolBObjectCount, maximumPlaces = -1):
+	totalCountIncludingTreble = poolAObjectCount + poolBObjectCount + 2
 
+	fullChange = '.' * totalCountIncludingTreble
+	print(fullChange)
 
-print("maximumSwapsEverywhere = ", maximumSwapsEverywhere)
-print("minimumSwapsEverywhere = ", minimumSwapsEverywhere, " (to prevent >", maximumPlaces, "places being made)")
-print("")
-print("maximumSwapsPoolsA = ", maximumSwapsPoolsA)
-print("maximumSwapsPoolsB = ", maximumSwapsPoolsB)
+	totalObjects = poolAObjectCount + poolBObjectCount
 
-print("")
-print("")
+	# we need to ensure pool A count <= pool B count as algorithm
+	# assume this; swap them if necessary.
 
-print(" <><><><><><><> have swap ranges: ", minimumSwapsEverywhere, maximumSwapsEverywhere)
-
-totalPossibilities = 0
-
-for swaps in range(minimumSwapsEverywhere, maximumSwapsEverywhere + 1):
-	print('')
-	print("generating", swaps, "swaps:")
-
-	if swaps == 0:
-		print("Skipping 0 swaps, it does nothing, adding 1 possibility")
-		totalPossibilities += 1
-		continue
-
-	shortfallOfSwapsInPoolB = swaps - maximumSwapsPoolsB
-
-	minimumSwapsPoolA = max(0, shortfallOfSwapsInPoolB)
+	if poolBObjectCount < poolAObjectCount:
+		poolAObjectCount, poolBObjectCount = poolBObjectCount, poolAObjectCount
 
 	print("")
-	print("shortfallOfSwapsInPoolB = %d, so minimumSwapsPoolA = %d" % (shortfallOfSwapsInPoolB, minimumSwapsPoolA))
+	print(" Pool A size:", poolAObjectCount)
+	print(" Pool B size:", poolBObjectCount)
+	print("")
 
-	for swapsInA in range(minimumSwapsPoolA, maximumSwapsPoolsA + 1):
-		print("--> doing poolA swaps:", swapsInA)
-		swapsInB = swaps - swapsInA
-		print("    --> so need", swapsInB, "swaps in B")
 
-		reducedPoolSizeA = poolAObjectCount - swapsInA
-		reducedPoolSizeB = poolBObjectCount - swapsInB
+	maximumSwapsPoolsA = poolAObjectCount // 2 
+	maximumSwapsPoolsB = poolBObjectCount // 2
 
-		changeWithTreble = fullChange[0:reducedPoolSizeA] + '><' + fullChange[0:reducedPoolSizeB]
+	maximumSwapsEverywhere = maximumSwapsPoolsA + maximumSwapsPoolsB
 
-		# changeWithTreble[poolAObjectCount - 1] = '>'
-		# changeWithTreble[poolAObjectCount] = '<'
+	if maximumPlaces >= 0:
+		minimumSwapsEverywhere = max(0, (totalObjects - maximumPlaces) // 2)
+	else:
+		minimumSwapsEverywhere = 0
 
-		print("%s   " % changeWithTreble, end='')
+	print(" maximumSwapsEverywhere = ", maximumSwapsEverywhere)
+	print(" minimumSwapsEverywhere = ", minimumSwapsEverywhere, " (to prevent >", maximumPlaces, "places being made)")
+	print("")
+	print(" maximumSwapsPoolsA = ", maximumSwapsPoolsA)
+	print(" maximumSwapsPoolsB = ", maximumSwapsPoolsB)
 
-		combosForA = calcCombos(reducedPoolSizeA, swapsInA)
-		combosForB = calcCombos(reducedPoolSizeB, swapsInB)
+	print("")
+	print("")
 
-		print("A combos = %d_C_%d = %d; " % (reducedPoolSizeA, swapsInA, combosForA), end='')
-		print("B combos = %d_C_%d = %d" % (reducedPoolSizeB, swapsInB, combosForB))
+	print(" <><><><><><><> have swap ranges: ", minimumSwapsEverywhere, maximumSwapsEverywhere)
 
-		possibilities = combosForA * combosForB
-		totalPossibilities += possibilities
-		print("->->-> so possibilities for this pair is ", possibilities)
+	totalPossibilities = 0
 
-	# swapsWantedInB = 
 
-print("")
-print("TOTAL POSSIBILITIES:", totalPossibilities)
 
+	for swaps in range(minimumSwapsEverywhere, maximumSwapsEverywhere + 1):
+		print('')
+		print("   generating", swaps, "swaps:")
+
+		if swaps == 0:
+			print("   Skipping 0 swaps, it does nothing, adding 1 possibility")
+			totalPossibilities += 1
+			continue
+
+		shortfallOfSwapsInPoolB = swaps - maximumSwapsPoolsB
+
+		minimumSwapsPoolA = max(0, shortfallOfSwapsInPoolB)
+
+		print("")
+		print("   shortfallOfSwapsInPoolB = %d, so minimumSwapsPoolA = %d" % (shortfallOfSwapsInPoolB, minimumSwapsPoolA))
+
+		# for swapsInA in range(minimumSwapsPoolA, maximumSwapsPoolsA + 1):
+		for swapsInA in range(minimumSwapsPoolA, min(maximumSwapsPoolsA + 1, swaps)): # second term incorrect
+			print("    --> doing poolA swaps:", swapsInA)
+			swapsInB = swaps - swapsInA
+			print("       --> so need", swapsInB, "swaps in B")
+
+			if swapsInB < 0:
+				print('\n\n')
+				sys.exit("ABORT: found < 0 swaps in B!\n\n")
+
+			reducedPoolSizeA = poolAObjectCount - swapsInA
+			reducedPoolSizeB = poolBObjectCount - swapsInB
+
+			changeWithTreble = fullChange[0:reducedPoolSizeA] + '><' + fullChange[0:reducedPoolSizeB]
+			print("    %s   " % changeWithTreble, end='')
+
+			combosForA = calcCombos(reducedPoolSizeA, swapsInA)
+			combosForB = calcCombos(reducedPoolSizeB, swapsInB)
+
+			print("   A combos = %d_C_%d = %d; " % (reducedPoolSizeA, swapsInA, combosForA), end='')
+			print("   B combos = %d_C_%d = %d" % (reducedPoolSizeB, swapsInB, combosForB))
+
+			possibilities = combosForA * combosForB
+			totalPossibilities += possibilities
+			print("   ->->-> so possibilities for this pair is ", possibilities)
+
+		# swapsWantedInB = 
+
+		print("")
+		print("   TOTAL POSSIBILITIES:", totalPossibilities)
+	return totalPossibilities
+
+stage = 7
+
+evenResults = []
+oddResults = []
+
+maxPlaces = -1
+
+# splitCombosCount = calcSplitCombosCount(0, 8, maxPlaces)
+# splitCombosCount = calcSplitCombosCount(1, 7, maxPlaces)
+# splitCombosCount = calcSplitCombosCount(2, 6, maxPlaces)
+# splitCombosCount = calcSplitCombosCount(3, 5, maxPlaces)
+# splitCombosCount = calcSplitCombosCount(4, 4, maxPlaces)
+# splitCombosCount = calcSplitCombosCount(5, 3, maxPlaces)
+# splitCombosCount = calcSplitCombosCount(6, 2, maxPlaces)
+# splitCombosCount = calcSplitCombosCount(7, 1, maxPlaces)
+# splitCombosCount = calcSplitCombosCount(8, 0, maxPlaces)
+
+# splitCombosCount = calcSplitCombosCount(5, 4, maxPlaces)
+# splitCombosCount = calcSplitCombosCount(4, 5, maxPlaces)
+
+for poolASize in range(0, stage - 2 + 1):
+	print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= poolASize:", poolASize)
+	poolBSize = stage - 2 - poolASize
+
+	splitCombosCount = calcSplitCombosCount(poolASize, poolBSize, maxPlaces)
+
+	if poolASize % 2 == 0:
+		evenResults.append(splitCombosCount)
+	else:
+		oddResults.append(splitCombosCount)
+
+	print("ANSWER:", splitCombosCount)
+
+print("evens:", evenResults)
+print("odds:", oddResults)
+
+all = list(zip(evenResults, oddResults))
+allFlatten = lambda l: [item for sublist in l for item in sublist]
+
+print("all:", allFlatten(all))
